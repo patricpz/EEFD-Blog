7eea9944018cbb805a6b61640de931e80289061f
patricpz <patricbarros19@gmail.com>
2025-09-12 08:52:01 -0300

    next auth secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "./prisma";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
